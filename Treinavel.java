4c0d9eb6397aca5018191002a40f421b2f2e5303
Rodrigo Pontes <rodrigopontezz@gmail.com>
2018-04-15 22:11:50 -0300

    Forçando merge

--- Treinavel.java
+++
@@ -1,9 +0,0 @@
-/*	 Interface que identifica todos os objetos que podem ser treinados pelos neurônios.
- * 
- * 	 @author Rodrigo Pontes
- *	 @version 1.1
- */
-
-public interface Treinavel {
-	public String getTarget();
-}
